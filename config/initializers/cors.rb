f5f2eb3a28d5e4ef640819f3445c1bda41879c57
sagarsikha12 <68449419+sagarsikha12@users.noreply.github.com>
2023-11-22 08:35:49 +1100

    updated the frontend in cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Adjust this to your React frontend's domain/port
+    origins 'https://sitecampaign-e61rbaibt-sagarsikha12s-projects.vercel.app' # Adjust this to your React frontend's domain/port
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 end
