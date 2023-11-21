1373863ab8e5225e1ee30e501632371c2d4971ad
sagarsikha12 <68449419+sagarsikha12@users.noreply.github.com>
2023-11-21 20:20:20 +1100

    origin to all in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '127.0.0.1:3000' # Adjust this to your React frontend's domain/port
+    origins '*' # Adjust this to your React frontend's domain/port
 
     resource '*',
       headers: :any,
